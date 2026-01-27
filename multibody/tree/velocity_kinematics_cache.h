93dfd4abebf65665dd259cd7323109268be386d8
Michael Sherman <sherm1@gmail.com>
2026-01-26 18:00:24 -0800

    Working on velocity cache.

--- multibody/tree/velocity_kinematics_cache.h
+++ multibody/tree/velocity_kinematics_cache.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+#include "multibody_tree_indexes.h"
+
 #include "drake/common/default_scalars.h"
 #include "drake/common/drake_assert.h"
 #include "drake/common/drake_copyable.h"
@@ -16,20 +18,32 @@ namespace internal {
 
 // TODO(sherm1) Should cache qdot here.
 
-// This class is one of the cache entries in the Context. It holds the
-// kinematics results of computations that depend not only on the generalized
-// positions of the system, but also on its generalized velocities.
-// Velocity kinematics results include:
-//
-// - Spatial velocity `V_WB` for each body B in the model as measured and
-//   expressed in the world frame W.
-// - Spatial velocity `V_PB` for each body B in the model as measured and
-//   expressed in the inboard (or parent) body frame P.
-// - Spatial velocity `V_FMb_W` of frame Mb measured in the inboard frame F and
-//   expressed in W. Mb is an "offset" frame rigidly fixed to M, whose axes are
-//   parallel to M but whose origin is at Bo rather than Mo.
-//
-// @tparam_default_scalar
+/* This class is one of the cache entries in the Context. It holds the
+kinematics results of computations that depend not only on the generalized
+positions of the system, but also on its generalized velocities.
+
+Velocity kinematics results are mostly per-Mobod. We also need to know the
+velocity of every Link (composite mobods carry multiple links). Note that
+every mobod B (composite or not) has a distinguished "active" link L₀, which
+is the most inboard link following that mobod (that is, the link with the
+joint that connects the mobod to its inboard mobod). The body frame B of
+a mobod is always coincident with the frame L₀ of its active link.
+
+- V_WB:   Spatial velocity of mobod B measured and expressed in the world
+          frame W. Frame B is the same as frame L₀ of a mobod's active link.
+- V_WL:   Spatial velocity link L in W for every link. Indexed by
+          LinkOrdinal. Same as V_WB if L is the active link of mobod B.
+- V_PB_W: Spatial velocity of mobod B measured in its parent (inboard)
+          mobod's frame P, expressed in W.
+- V_FM:   Spatial velocity of the mobilizer's outboard frame M in its inboard
+          frame F, expressed in F.
+
+@tparam_default_scalar */
+
+// TODO(sherm1) V_WL mostly duplicates V_WB (they are identical if there are no
+//  composite bodies), and always contains all the V_WB spatial velocities (for
+//  the active links). Consider replacing V_WB(mobod) with
+//  V_WL(mobod.active_link) to avoid duplication.
 template <typename T>
 class VelocityKinematicsCache {
  public:
@@ -42,11 +56,12 @@ class VelocityKinematicsCache {
   // so that operations using this uninitialized cache entry fail fast, easing
   // bug detection.
   explicit VelocityKinematicsCache(const SpanningForest& forest)
-      : num_mobods_(forest.num_mobods()) {
+      : num_mobods_(forest.num_mobods()), num_links_(forest.num_links()) {
     Allocate();
     DRAKE_ASSERT_VOID(InitializeToNaN());
     // Sets defaults.
     V_WB_pool_[world_mobod_index()].SetZero();   // World's velocity is zero.
+    V_WL_pool_[world_link_ordinal()].SetZero();  //           "
     V_FM_pool_[world_mobod_index()].SetNaN();    // It must never be used.
     V_PB_W_pool_[world_mobod_index()].SetNaN();  // It must never be used.
   }
@@ -58,6 +73,9 @@ class VelocityKinematicsCache {
       V_WB_pool_[mobod_index].SetZero();
       V_FM_pool_[mobod_index].SetZero();
       V_PB_W_pool_[mobod_index].SetZero();
+    }
+    for (LinkOrdinal link_ordinal(0); link_ordinal < num_links_; ++link_ordinal) {
+      V_WL_pool_[link_ordinal].SetZero();
     }
   }
 
@@ -75,6 +93,16 @@ class VelocityKinematicsCache {
   SpatialVelocity<T>& get_mutable_V_WB(MobodIndex mobod_index) {
     DRAKE_ASSERT(0 <= mobod_index && mobod_index < num_mobods_);
     return V_WB_pool_[mobod_index];
+  }
+
+  const SpatialVelocity<T>& get_V_WL(LinkOrdinal ordinal) const {
+    DRAKE_ASSERT(0 <= ordinal && ordinal < num_links_);
+    return V_WL_pool_[ordinal];
+  }
+
+  void SetX_WL(LinkOrdinal ordinal, const SpatialVelocity<T>& V_WL) {
+    DRAKE_DEMAND(0 <= ordinal && ordinal < num_links_);
+    V_WL_pool_[ordinal] = V_WL;
   }
 
   // Returns a const reference to the across-mobilizer (associated with
@@ -106,19 +134,12 @@ class VelocityKinematicsCache {
   }
 
  private:
-  // Pools store entries in the same order as the mobilized bodies (BodyNodes)
-  // in the multibody forest, i.e. in DFT (Depth-First Traversal) order.
-  // Therefore clients of this class will access entries by MobodIndex, see
-  // `get_V_WB()` for instance.
-
-  // The type of the pools for storing spatial velocities.
-  typedef std::vector<SpatialVelocity<T>> SpatialVelocity_PoolType;
-
-  // Allocates resources for this position kinematics cache.
+  // Allocates resources for this velocity kinematics cache.
   void Allocate() {
     V_WB_pool_.resize(num_mobods_);
     V_FM_pool_.resize(num_mobods_);
     V_PB_W_pool_.resize(num_mobods_);
+    V_WL_pool_.resize(num_links_);
   }
 
   // Initializes all pools to have NaN values to ease bug detection when entries
@@ -129,13 +150,25 @@ class VelocityKinematicsCache {
       V_FM_pool_[mobod_index].SetNaN();
       V_PB_W_pool_[mobod_index].SetNaN();
     }
+    for (LinkOrdinal link_ordinal(0); link_ordinal < num_links_; ++link_ordinal) {
+      V_WL_pool_[link_ordinal].SetNaN();
+    }
   }
 
-  // Number of body nodes in the corresponding MultibodyTree.
+  // Number of Mobods in the multibody forest, including the World mobod.
   int num_mobods_{0};
-  SpatialVelocity_PoolType V_WB_pool_;
-  SpatialVelocity_PoolType V_FM_pool_;
-  SpatialVelocity_PoolType V_PB_W_pool_;
+
+  // Number of links in the multibody graph includes all user-defined links
+  // (including the World link) and possibly some ephemeral links.
+  int num_links_{0};
+
+  // These are indexed by MobodIndex so are in depth-first order.
+  std::vector<SpatialVelocity<T>> V_WB_pool_;
+  std::vector<SpatialVelocity<T>> V_FM_pool_;
+  std::vector<SpatialVelocity<T>> V_PB_W_pool_;
+
+  // This pool is indexed by LinkOrdinal.
+  std::vector<SpatialVelocity<T>> V_WL_pool_;
 };
 
 }  // namespace internal
